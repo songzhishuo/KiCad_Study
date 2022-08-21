47307e26c3949568b02e614da5fc83e5fd918c21
songzhishuo <815794369@qq.com>
2022-08-21 13:47:29 +0800

    Add: 添加操作文档

---
+++ 2.Doc/快捷键和使用技巧.mk
@@ -0,0 +1,68 @@
+# 快捷键记录
+
+## 常规操作快捷键
+
+复制 `Ctrl+C`
+
+粘贴 `Ctrl+V`
+
+移动 `M`
+
+拖动 `M`
+
+标签 `l`
+
+符号属性设置 `E`
+
+拉线 `l`
+
+
+
+## 库操作快捷键
+
+元器件库 `a`
+
+电源符号库 `p`
+
+
+
+封装设置：![image-20220820221618881](E:\kicad\2.Doc\image\封装配置.png)
+
+ERC检查: ![image-20220820221708955](E:\kicad\2.Doc\image\ERC.png)
+
+
+
+## 修改Style
+
+[(70条消息) KiCad主题和背景更改的方法_本物の物語的博客-CSDN博客](https://blog.csdn.net/qq_44504673/article/details/105535906)
+
+# PCB绘制流程
+
+1. 设置原点
+2. 绘制板框 Edge.Cuts 
+3. 绘制电路边界层 Margin （和板框间距0.3mm）
+4. 布局
+5. 修改丝印位置
+6. 设置电路板规则
+
+# PCB 操作快捷键
+
+切换走线  `/`
+
+切换图层  `v`
+
+# PCB 参数配置
+
+## 线宽配置
+
+![image-20220820230054069](E:\kicad\2.Doc\image\电路板规则.png)
+
+![image-20220820230509876](E:\kicad\2.Doc\image\网络定义.png)
+
+## 阻焊配置
+![image-20220820230618659](E:\kicad\2.Doc\image\阻焊设置.png)
+
+## 铺铜配置
+
+![image-20220820233551733](C:\Users\songz\AppData\Roaming\Typora\typora-user-images\image-20220820233551733.png)
+
